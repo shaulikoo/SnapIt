b85c291ce4edb6cb77a880c1a95943f8be9b2eff
shaulikoo <shaulikoo@gmail.com>
2015-08-17 22:54:52 +0300

    Home-Changed

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('passIn'));
-app.use(session());
+app.use(session({
+  secret: 'passIn',
+  resave : true,
+  saveUninitialized: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next){
   var err = req.session.error
