b8e669f129039b78406f206b374b9ebe7208fb8b
shaulikoo <shaulikoo@gmail.com>
2015-08-24 14:41:58 +0300

    Home-Changed

--- routes/camera.js
+++ routes/camera.js
@@ -6,7 +6,7 @@ var ok;
 
 /* GET camera page. */
 router.get('/', function(req, res, next){
-    ok = camera.pico()
+    ok = camera.one()
     res.render('wait',{title: 'SnapIt-Hw', time: '4'});
 });
 
